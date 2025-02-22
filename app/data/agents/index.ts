47cf5d6a4af101bf4997acd9bd1f67f058866198
sid <35936587+siddharth-shringarpure@users.noreply.github.com>
2025-02-22 16:37:05 +0000

    ✨ Add initial agents

---
+++ app/data/agents/index.ts
@@ -0,0 +1,8 @@
+export { TOPIC_MAPPER_INSTRUCTIONS } from './topicMapper';
+export { EXPLAINER_INSTRUCTIONS } from './explainer';
+export { QUIZ_MASTER_INSTRUCTIONS } from './quizMaster';
+export { FEEDBACK_INSTRUCTIONS } from './feedback';
+export { RETENTION_INSTRUCTIONS } from './retention';
+export { SUMMARIZER_INSTRUCTIONS } from './summarizer';
+export { ORCHESTRATOR_INSTRUCTIONS } from './orchestrator';
+export { SAFETY_AGENT_INSTRUCTIONS } from './safetyAgent'; 
